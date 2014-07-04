a9384947c5f3b353e65a091c57b99f83e9dd0845
Bram Van Rensbergen <mail@bramvanrensbergen.com>
2014-07-04 15:26:28 +0200

    Added all association cues. Some code cleanup.

--- core/js/associations.js
+++ core/js/associations.js
@@ -1,38 +1,82 @@
-/**
- * Return the indicated array, with all elements shuffled.
+CUES = [];  	//will hold the cues the participant will see, in order
+RESPONSES = []; //will hold participant's responses to the association experiment
+
+/*
+ * A note on cues and cue order.
+ * Cues consist of 4 names, and 18 words --> a total of 22 cues.
+ * 2 names are local, 2 names are foreign.
+ * 6 words have a very high valence, 6 have a neutral valence, 6 have a negative valence
+ * 
+ * Position of the names is not random: there will be a name at position 6, 11, 16, and 21
+ * Names take turns; either local, foreign, local, foreign; or, foreign, local, foreign, local
+ * 
+ * The order of the rest of the words is fully randomized.
+ * 
+ * All cues are hardcoded in this function. 
+ * Each cue is accompagnied by their type: valence level i.e. positive/negative/neutral for words, and local/foreign for names.
  */
-function shuffle(array) {
-    var counter = array.length, temp, index;
 
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        index = Math.floor(Math.random() * counter);
+function initializeAssociations() {		
+	//the 'regular' words that will be presented, in the order they will be in the final CUES
+	var cueWords = shuffle([
+	        ['Vriend', 'positive'],
+	        ['Betrouwbaar', 'positive'],
+	        ['Heerlijk', 'positive'],
+	        ['Liefde', 'positive'],
+	        ['Prachtig', 'positive'],
+	        ['Eerlijk', 'positive'],
+	        ['Drempel', 'neutral'],
+	        ['Olie', 'neutral'],
+	        ['Kruimel', 'neutral'],
+	        ['Kever', 'neutral'],
+	        ['Steen', 'neutral'],
+	        ['Knoop', 'neutral'],
+	        ['Ongelukkig', 'negative'],
+	        ['Failliet', 'negative'],
+	        ['Vijand', 'negative'],
+	        ['Corrupt', 'negative'],
+	        ['Crimineel', 'negative'],
+	        ['Agressie', 'negative']
+	]);
 
-        // Decrease counter by 1
-        counter--;
+	var localNames = shuffle([
+			['Niels', 'local'],
+			['Nathalie', 'local'],
+	]);
 
-        // And swap the last element with it
-        temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
-
-    return array;
-}
-
-function initializeAssociations() {	
-	//Cues that will be presented in the association task. 
-	//Second value is the cue's category, which will be written to the output file next to the associations.
+	var foreignNames = shuffle([
+			['Mohammed', 'foreign'],
+			['Fatima', 'foreign']
+	]);		
+		
+	CUES = cueWords;
 	
-	//Niels, Nathalie, Emma, Vincent, Bart, Thomas, Peter, An, Katrien, Nathalie
-	//Mohammed, Loubna, Malika, Rachida, Rachid, Abdel, Ahmed, Samir, Fatima, Ayisha
-	cue_list = [['Niels', 'vlaams'],
-                ['Emma', 'vlaams'],
-                ['Mohammed', 'vreemd'],
-                ['Rachida', 'vreemd']];
-	cue_list = shuffle(cue_list); //shuffle the cues
-	association_responses = new Array(); //will hold participant's responses to the association experiment
+	localFirst = Math.random() > 0.5; //true: local at 7 and 16, foreign at 11 and 21; false: vice versa
+	CUES.splice(5,  0, localFirst ? localNames[0]   : foreignNames[0] );
+	CUES.splice(10, 0, localFirst ? foreignNames[0] : localNames[0]   );
+	CUES.splice(15, 0, localFirst ? localNames[1]   : foreignNames[1] );
+	CUES.splice(20, 0, localFirst ? foreignNames[1] : localNames[1]   );	
+	//note: these splice commands insert the element at that index, shifting the rest of the array one element forward
+	
+	/**
+	 * Return the indicated array, with all elements shuffled.
+	 */
+	function shuffle(array) {
+		var counter = array.length, temp, index;
+
+		// While there are elements in the array
+		while (counter > 0) {
+
+			index = Math.floor(Math.random() * counter); // Pick a random index
+			counter--;
+
+			// Swap the last element with counter
+			temp = array[counter];
+			array[counter] = array[index];
+			array[index] = temp;
+		}
+		return array;
+	}	
 }
 
 
@@ -68,16 +112,16 @@ function showAssociationTask() {
  */
 function startAssociationTask() {
 	var cue = '';		//the cue the ss is giving associations to
-	var cue_cat = '';	//category of the cue, e.g. 'local name' or 'foreign name'
-	var trial_nb = 0;	//trial number; 1 .. number of cues (i.e., 3 associations to each cue are all the same trial)
-	var asso_index = 1;	//1, 2, or 3, depending on whether the association is the first, second, or third association to the current cue
+	var cueCat = '';	//category of the cue, e.g. 'local name' or 'foreign name'
+	var trialNb = 0;	//trial number; 1 .. number of cues (i.e., 3 associations to each cue are all the same trial)
+	var assoIndex = 1;	//1, 2, or 3, depending on whether the association is the first, second, or third association to the current cue
 	
 	//for each association, we'll save RT to first keypress and RT to submit
 	//measured from when cue is displayed or when previous association was submitted, whichever is later
-	var RT_start = -1;					 
-	var RT_to_submit = -1;
-	var RT_to_keypress = -1;
-	var first_keypress_registered = false;
+	var RtStart = -1;					 
+	var RtToSubmit = -1;
+	var RtToKeypress = -1;
+	var firstKeypressRegistered = false;
 	
 	jQuery('#associations__submit').bind('click',function(e) {	//triggered by 'enter' or clicking 'ok'
 		if(e) e.preventDefault();								//make sure we stay on this page
@@ -92,9 +136,9 @@ function startAssociationTask() {
 	});
 	
 	$("#associations__response").keypress(function() {
-		if (!first_keypress_registered) {
-			first_keypress_registered = true;
-			RT_to_keypress = new Date().getTime() - RT_start;
+		if (!firstKeypressRegistered) {
+			firstKeypressRegistered = true;
+			RtToKeypress = new Date().getTime() - RtStart;
 		}
 	});
 	
@@ -102,32 +146,32 @@ function startAssociationTask() {
 	 * Write away response, move on to next association or cue.
 	 */	
 	function processAssociation(association) {
-		RT_to_submit = new Date().getTime() - RT_start;
+		RtToSubmit = new Date().getTime() - RtStart;
 				
 		//start by saving the trial
-		association_responses.push({
+		RESPONSES.push({
 			cue: cue,
-			cue_cat: cue_cat,
-			trial_nb: trial_nb,
+			cueCat: cueCat,
+			trialNb: trialNb,
 			association: association,
-			asso_nb: asso_index,
-			RT_to_keypress: RT_to_keypress,
-			RT_to_submit: RT_to_submit
+			assoNb: assoIndex,
+			RtToKeypress: RtToKeypress,
+			RtToSubmit: RtToSubmit
 		});
 		
 		//allow for RT measurement of next association
-		first_keypress_registered = false;
-		RT_start = new Date().getTime(); 
+		firstKeypressRegistered = false;
+		RtStart = new Date().getTime(); 
 		
 		//show next association, or next cue
-		if (asso_index == 1) { //this was ss's first association
+		if (assoIndex == 1) { //this was ss's first association
 			$("#associations__previous--1").html(association); //display this association while ss thinks of further associations
 			$("#associations__response").attr("placeholder", "Geef een tweede associatie");		
-			asso_index++;
-		} else if (asso_index == 2) { 	//this was ss's second association
+			assoIndex++;
+		} else if (assoIndex == 2) { 	//this was ss's second association
 			$("#associations__previous--2").html(association); //display this association while ss thinks of further associations
 			$("#associations__response").attr("placeholder", "Geef een laatste associatie");
-			asso_index++;
+			assoIndex++;
 		} else {
 			$("#associations__response").attr("placeholder", "Geef een eerste associatie");
 			showNextCue(); //pp gave three associations -> move to next cue
@@ -140,14 +184,14 @@ function startAssociationTask() {
 	function showNextCue() {
 		$(".associations__previous").html("...");		
 		
-		if (cue_list.length > 0) {
-			var cue_entry = cue_list.shift();
-			cue = cue_entry[0];
-			cue_cat = cue_entry[1];
-			trial_nb++;
-			asso_index = 1;
+		if (CUES.length > 0) {
+			var cueEntry = CUES.shift();
+			cue = cueEntry[0];
+			cueCat = cueEntry[1];
+			trialNb++;
+			assoIndex = 1;
 			$("#associations__cue").html(capitalizeFirst(cue));		
-			RT_start = new Date().getTime(); //reset RT
+			RtStart = new Date().getTime(); //reset RT
 		} else {
 			WriteAssociationData();
 			showIATInstructions();
@@ -158,7 +202,7 @@ function startAssociationTask() {
 		return string.charAt(0).toUpperCase() + string.slice(1);
 	}
 	
-	trial_nb = 0;
+	trialNb = 0;
 	showNextCue();	
 }
 
@@ -167,11 +211,11 @@ function WriteAssociationData(){
 	var subject = sub;
 	subject = subject.length==0 ? "unknown" : subject;
 	subject = "ASSO-" + subject;
-	var str = "cue\tcue_cat\ttrial_nb\tasso\tasso_nb\tRT_firstkey\tRT_submit\n";	
+	var str = "cue\tcueCat\ttrialNb\tasso\tassoNb\tRT_firstkey\tRT_submit\n";	
 	
-	for (var i = 0; i < association_responses.length; i++) {
-		var t = association_responses[i];
-		str = str + t.cue + "\t" + t.cue_cat + "\t" + t.trial_nb + "\t" + t.association + "\t" + t.asso_nb + "\t" + t.RT_to_keypress + "\t" + t.RT_to_submit + "\n";		
+	for (var i = 0; i < RESPONSES.length; i++) {
+		var t = RESPONSES[i];
+		str = str + t.cue + "\t" + t.cueCat + "\t" + t.trialNb + "\t" + t.association + "\t" + t.assoNb + "\t" + t.RtToKeypress + "\t" + t.RtToSubmit + "\n";		
 	}	
 
 	$.post("core/fileManager.php", { 'op':'writeoutput', 'template':template.name, 
